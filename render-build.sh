b3506b121011db54d414a0534b6cb8e83b0a656d
Tafitaniaina rtm <harenabainina@gmail.com>
2026-03-07 09:10:23 +0300

    Add Chrome download and executable check
    Added download and extraction steps for Chrome binary.

--- render-build.sh
+++ render-build.sh
@@ -3,7 +3,9 @@ set -o errexit
 
 npm install
 
-# Installe Chrome pour Linux (Render)
+# Téléchargement
 wget https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.94/linux64/chrome-linux64.zip
 unzip -o chrome-linux64.zip
-rm chrome-linux64.zip
+
+# Vérification : on s'assure que le binaire est exécutable
+chmod +x chrome-linux64/chrome
